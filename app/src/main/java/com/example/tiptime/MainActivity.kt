d338244fa32fcd53cdfbedfcbf2278b2224f0682
Kaizoku22 <pdeepankar46@gmail.com>
2023-11-29 00:04:12 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
-    var amountInput by remember {mutableStateOf("0") }
+    
     Column(
         modifier = Modifier
             .statusBarsPadding()
